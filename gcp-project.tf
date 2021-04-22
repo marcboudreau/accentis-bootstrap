e0e0e62307d5d8b00bb5f142033a0925404a9c5f
marcboudreau <marc.boudreau@solace.com>
2021-04-22 17:50:20 -0400

    Initial gcp-project Terraform configuration

--- gcp-project.tf
+++ gcp-project.tf
@@ -30,26 +30,61 @@ resource "tfe_workspace" "gcp_project" {
     execution_mode     = "remote"
 
     vcs_repo {
-        identifier   = "marcboudreau/accentis-gcp-project"
-        description  = "A Terraform project containing the configuration for project-wide resources in GCP Projects used for Accentis"
-        visibility   = "private"
-        has_issues   = true
-        has_projects = false
-
-        archived     = false
-
-        allow_merge_commit = false
-        allow_squash_merge = true
-        allow_rebase_merge = false
-
-        delete_branch_on_merge = true
-        license_template       = "mit"
-
-        topics = [
-            "terraform",
-            "gcp",
-            "iam",
-            "kms",
-        ]
+        identifier     = "marcboudreau/accentis-gcp-project"
+        branch         = "main"
+        oauth_token_id = ""
     }
+}
+
+resource "tfe_variable" "gcp_project_project_id" {
+    for_each = toset(local.project_ids)
+
+    key          = "project_id"
+    value        = "accentis-${each.value}"
+    category     = "terraform"
+    workspace_id = tfe_workspace.gcp_project[each.value].id
+}
+
+resource "tfe_variable" "gcp_project_default_sa" {
+    for_each = toset(local.project_ids)
+
+    key          = "default_compute_engine_service_account"
+    value        = var.gcp_default_compute_engine_service_accounts["accentis-${each.value}"]
+    category     = "terraform"
+    workspace_id = tfe_workspace.gcp_project[each.value].id
+}
+
+resource "tfe_variable" "gcp_project_credentials" {
+    for_each = toset(local.project_ids)
+
+    key          = "GOOGLE_CREDENTIALS"
+    value        = base64decode(var.gcp_credentials[each.value])
+    category     = "env"
+    sensitive    = true
+    description  = "Service Account Key used to make GCP API calls"
+    workspace_id = tfe_workspace.gcp_project[each.value].id
+}
+
+resource "github_repository" "gcp_project" {
+    name         = "accentis-gcp-project"
+    description  = "A Terraform project containing the configuration for project-wide resources in GCP Projects used for Accentis"
+    visibility   = "private"
+    has_issues   = true
+    has_projects = false
+
+    archived     = false
+
+    allow_merge_commit = false
+    allow_squash_merge = true
+    allow_rebase_merge = false
+
+    delete_branch_on_merge = true
+    license_template       = "mit"
+
+    topics = [
+        "terraform",
+        "gcp",
+        "iam",
+        "kms",
+    ]
 }
